5b1c4d2028ff717da1e394a3777137d1acaa2c1c
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2020-12-22 06:02:50 -0800

    update
    chats

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const Chats = ()=> {
