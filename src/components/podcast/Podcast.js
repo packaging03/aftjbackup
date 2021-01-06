46fe51dbe37bd117a9122b286a16177546c4b7a7
peter2c <peter2c@yahoo.com>
2021-01-06 19:22:28 +0100

    AFTj

--- src/components/podcast/Podcast.js
+++ src/components/podcast/Podcast.js
@@ -40,52 +40,52 @@ const Podcast = ({navigation}) => {
   const isPlayerReady = useRef(false);
   const index = useRef(0);
 
-  const [songs, setSong] = useState([]);
+  // const [songs, setSong] = useState([]);
   const [songIndex, setSongIndex] = useState(0);
-  // const [songs, setSong] = useState([
-  //   {
-  //     title: 'Death Bed',
-  //     artist: 'Powfu',
-  //     artwork: 'https://samplesongs.netlify.app/album-arts/death-bed.jpg',
-  //     url: 'https://samplesongs.netlify.app/Death%20Bed.mp3',
-  //     id: '1',
-  //   },
-  //   {
-  //     title: 'Bad Liar',
-  //     artist: 'Imagine Dragons',
-  //     artwork: 'https://samplesongs.netlify.app/album-arts/bad-liar.jpg',
-  //     url: 'https://samplesongs.netlify.app/Bad%20Liar.mp3',
-  //     id: '2',
-  //   },
-  //   {
-  //     title: 'Faded',
-  //     artist: 'Alan Walker',
-  //     artwork: 'https://samplesongs.netlify.app/album-arts/faded.jpg',
-  //     url: 'https://samplesongs.netlify.app/Faded.mp3',
-  //     id: '3',
-  //   },
-  //   {
-  //     title: 'Hate Me',
-  //     artist: 'Ellie Goulding',
-  //     artwork: 'https://samplesongs.netlify.app/album-arts/hate-me.jpg',
-  //     url: 'https://samplesongs.netlify.app/Hate%20Me.mp3',
-  //     id: '4',
-  //   },
-  //   {
-  //     title: 'Solo',
-  //     artist: 'Clean Bandit',
-  //     artwork: 'https://samplesongs.netlify.app/album-arts/solo.jpg',
-  //     url: 'https://samplesongs.netlify.app/Solo.mp3',
-  //     id: '5',
-  //   },
-  //   {
-  //     title: 'Without Me',
-  //     artist: 'Halsey',
-  //     artwork: 'https://samplesongs.netlify.app/album-arts/without-me.jpg',
-  //     url: 'https://samplesongs.netlify.app/Without%20Me.mp3',
-  //     id: '6',
-  //   },
-  // ]);
+  const [songs, setSong] = useState([
+    {
+      title: 'Death Bed',
+      artist: 'Powfu',
+      artwork: 'https://samplesongs.netlify.app/album-arts/death-bed.jpg',
+      url: 'https://samplesongs.netlify.app/Death%20Bed.mp3',
+      id: '1',
+    },
+    {
+      title: 'Bad Liar',
+      artist: 'Imagine Dragons',
+      artwork: 'https://samplesongs.netlify.app/album-arts/bad-liar.jpg',
+      url: 'https://samplesongs.netlify.app/Bad%20Liar.mp3',
+      id: '2',
+    },
+    {
+      title: 'Faded',
+      artist: 'Alan Walker',
+      artwork: 'https://samplesongs.netlify.app/album-arts/faded.jpg',
+      url: 'https://samplesongs.netlify.app/Faded.mp3',
+      id: '3',
+    },
+    {
+      title: 'Hate Me',
+      artist: 'Ellie Goulding',
+      artwork: 'https://samplesongs.netlify.app/album-arts/hate-me.jpg',
+      url: 'https://samplesongs.netlify.app/Hate%20Me.mp3',
+      id: '4',
+    },
+    {
+      title: 'Solo',
+      artist: 'Clean Bandit',
+      artwork: 'https://samplesongs.netlify.app/album-arts/solo.jpg',
+      url: 'https://samplesongs.netlify.app/Solo.mp3',
+      id: '5',
+    },
+    {
+      title: 'Without Me',
+      artist: 'Halsey',
+      artwork: 'https://samplesongs.netlify.app/album-arts/without-me.jpg',
+      url: 'https://samplesongs.netlify.app/Without%20Me.mp3',
+      id: '6',
+    },
+  ]);
 
   const isItFromUser = useRef(true);
 
@@ -227,7 +227,7 @@ const Podcast = ({navigation}) => {
       let json = await res.json();
       if (json.status === 'success') {
         console.log(json.Podcasts);
-        setSong(json.Podcasts);
+        // setSong(json.Podcasts);
       }
     } catch (e) {
       console.error(e);
